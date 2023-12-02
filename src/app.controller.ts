4ee28fea4ced5dcf4e7ccfd8176ba3586718dc9c
Darya Drachuk <darya_drachuk@epam.com>
2023-12-02 23:23:26 +0300

    feat: remove comment

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,45 +1,42 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Request, Post, UseGuards, HttpStatus } from '@nestjs/common';
+import { LocalAuthGuard, AuthService, JwtAuthGuard, BasicAuthGuard } from './auth';
 
 @Controller()
 export class AppController {
 
-  constructor() {}
-  @Get()
-  getHello(): string {
-    return 'sas!!!!!!!!!!!!!!!!!!!!'
+  constructor(private authService: AuthService) {}
+
+  @Get([ '', 'ping' ])
+  healthCheck(): any {
+    return {
+      statusCode: HttpStatus.OK,
+      message: 'OK',
+    };
   }
 
-  // @Get([ '', 'ping' ])
-  // healthCheck(): any {
-  //   return {
-  //     statusCode: HttpStatus.OK,
-  //     message: 'OK',
-  //   };
-  // }
+  @UseGuards(LocalAuthGuard)
+  @Post('api/auth/login')
+  async login(@Request() req) {
+    const token = this.authService.login(req.user, 'basic');
 
-  // @UseGuards(LocalAuthGuard)
-  // @Post('api/auth/login')
-  // async login(@Request() req) {
-  //   const token = this.authService.login(req.user, 'basic');
+    return  {
+      statusCode: HttpStatus.OK,
+      message: 'OK',
+      data: {
+        ...token,
+      },
+    };
+  }
 
-  //   return  {
-  //     statusCode: HttpStatus.OK,
-  //     message: 'OK',
-  //     data: {
-  //       ...token,
-  //     },
-  //   };
-  // }
-
-  // @UseGuards(BasicAuthGuard)
-  // @Get('api/profile')
-  // async getProfile(@Request() req) {
-  //   return {
-  //     statusCode: HttpStatus.OK,
-  //     message: 'OK',
-  //     data: {
-  //       user: req.user,
-  //     },
-  //   };
-  // }
+  @UseGuards(BasicAuthGuard)
+  @Get('api/profile')
+  async getProfile(@Request() req) {
+    return {
+      statusCode: HttpStatus.OK,
+      message: 'OK',
+      data: {
+        user: req.user,
+      },
+    };
+  }
 }
